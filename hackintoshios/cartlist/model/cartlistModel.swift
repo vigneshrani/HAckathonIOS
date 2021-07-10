32d8fe07d95a2ef5d17a342f506ccd026451c50f
vignesh <vignesh@vigneshs-iMac.local>
2021-07-10 14:31:24 +0530

    list api added

---
+++ hackintoshios/cartlist/model/cartlistModel.swift
@@ -0,0 +1,46 @@
+//
+//  cartlist.swift
+//  hackintoshios
+//
+//  Created by vignesh on 10/07/21.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct cartlistModel: Codable {
+    let error: Bool?
+    let message: String?
+    let orderItems: [OrderItem]?
+    let orderTotal: OrderTotal?
+}
+
+// MARK: - OrderItem
+struct OrderItem: Codable {
+    let orderID, itemID: Int?
+    let itemName: String?
+    let itemQuantity, itemPrice: Int?
+    let itemImage: String?
+
+    enum CodingKeys: String, CodingKey {
+        case orderID = "orderId"
+        case itemID = "itemId"
+        case itemName, itemQuantity, itemPrice, itemImage
+    }
+    init?(data: Data)
+    {
+        do
+        {
+            let jsonDecoder = JSONDecoder()
+            let decode = try jsonDecoder.decode(OrderItem.self, from: data)
+            self = decode
+        } catch {
+            return nil
+        }
+    }
+}
+
+// MARK: - OrderTotal
+struct OrderTotal: Codable {
+    let itemTotal, discount, toPay: Int?
+}
